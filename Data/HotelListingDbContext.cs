eb66e934332c967dab9fa499e6f8d663877560a3
MarwanMoussa88 <maromoussa88@gmail.com>
2023-09-19 14:51:37 +0300

    Added Database and seeded the tables

---
+++ Data/HotelListingDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+
+    //Entity Framework Core DB Context
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country { Id = 1, Name = "Egypt", ShortName = "EGY" },
+                new Country { Id = 2, Name = "America", ShortName = "USA" },
+                new Country { Id = 3, Name = "England", ShortName = "UK" });
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel { Id = 1, Address="Corniche St",Name="Sunrie Makadi",Rating=4.5,CountryId=1},
+                new Hotel { Id = 2, Address = "Mansheya", Name = "Winsdoor Palace", Rating =4 ,CountryId=1},
+                new Hotel { Id = 3, Address = "Montaza Palace", Name = "Helanan Palestine", Rating = 5 ,CountryId=1});   
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Get Connection String
+string? connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+//Add Db Context for entity framework 
+builder.Services.AddDbContext<HotelListingDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Cors (cross origin resource sharing)
-// it allows which origins are allowed to access its resources 
-// by deafault browsers implement (sop) same origin policy to prevent xss attacks 
+
+/*
+ * Cors (cross origin resource sharing)
+ * it allows which origins are allowed to access its resources
+ * by deafault browsers implement (sop) same origin policy to prevent xss attacks 
+ **/
 
 builder.Services.AddCors(options =>
 {
@@ -21,6 +32,8 @@ builder.Services.AddCors(options =>
     AllowAnyHeader());
 });
 
+
+//Adds serilog for logging
 builder.Host.UseSerilog((ctx,logger) => { 
     logger.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration);
 });
